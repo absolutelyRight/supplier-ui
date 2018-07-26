fb498ca738aacfcd43759d4a338506a05431a101
ladyDaiwei <davi_roxy@163.com>
2018-07-26 18:16:17 +0800

    消息通知页面的修改   ---davi

--- supplier-gateway/src/views/information/Notice.vue
+++ supplier-gateway/src/views/information/Notice.vue
@@ -3,280 +3,164 @@
 		<!--工具条-->
 		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
 			<el-form :inline="true" :model="filters">
-				<el-form-item>
-					<el-input v-model="filters.name" placeholder="姓名"></el-input>
-				</el-form-item>
-				<el-form-item>
+				<el-form-item style="float: right">
 					<el-button type="primary" v-on:click="getUsers">查询</el-button>
 				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="handleAdd">新增</el-button>
+				<el-form-item style="float: right">
+					<el-input v-model="filters.name" placeholder="输入关键字查询"></el-input>
 				</el-form-item>
 			</el-form>
 		</el-col>
 
 		<!--列表-->
 		<el-table :data="notices" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
-			<el-table-column prop="title" label="标题" min-width="30">
+			<el-table-column prop="title" label="标题" min-width="200">
 			</el-table-column>
-			<el-table-column prop="time" label="时间" min-width="30" sortable>
+			<el-table-column prop="time" label="时间" min-width="200" sortable>
 			</el-table-column>
-			<el-table-column label="详情" min-width="30">
+			<el-table-column label="详情" min-width="200">
 				<template slot-scope="scope">
 					<el-button size="small">查看详情</el-button>
 				</template>
 			</el-table-column>
-			<el-table-column prop="creater" label="发布人" min-width="30">
+			<el-table-column prop="creater" label="发布人" min-width="200">
 			</el-table-column>
-			<el-table-column label="操作" min-width="30">
+			<el-table-column label="操作" min-width="150">
 				<template slot-scope="scope">
 					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
 				</template>
 			</el-table-column>
 		</el-table>
 
-		<el-col :span="24" class="toolbar">
-			<el-pagination
-					background
-					layout="prev, pager, next"
-					:total="1000"
-					style="float: right">
-			</el-pagination>
-		</el-col>
+		<!--工具条-->
+
+		<el-pagination
+				small
+				layout="prev, pager, next"
+				:total="50"
+				style="float: right"
+		>
+		</el-pagination>
+
 	</section>
 </template>
 
 <script>
-	import util from '../../common/js/util'
-	//import NProgress from 'nprogress'
-	import { getUserListPage, removeUser, batchRemoveUser, editUser, addUser } from '../../api/api';
+    import util from '../../common/js/util'
+    //import NProgress from 'nprogress'
+    import { getUserListPage, removeUser, batchRemoveUser, editUser, addUser } from '../../api/api';
 
-	export default {
-		data() {
-			return {
-				filters: {
-					name: ''
-				},
+    export default {
+        data() {
+            return {
+                filters: {
+                    name: ''
+                },
                 notices: [
-					{
-						title: "品高软件xxx消息通知",
-						time: "2018-6-12 12:12:00",
-						creater: "小白"
-					},
                     {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
+                        title:"品高XX公告",
+                        time:"2018-7-26 15:50",
+                        creater:"完全正确"
                     },
                     {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
+                        title:"品高XX公告",
+                        time:"2018-7-26 15:50",
+                        creater:"完全正确"
                     },
                     {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
+                        title:"品高XX公告",
+                        time:"2018-7-26 15:50",
+                        creater:"完全正确"
                     },
                     {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
+                        title:"品高XX公告",
+                        time:"2018-7-26 15:50",
+                        creater:"完全正确"
                     },
                     {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
-                    },
-                    {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
-                    },
-                    {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
-                    },
-                    {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
-                    },
-                    {
-                        title: "品高软件xxx消息通知",
-                        time: "2018-6-12 12:12:00",
-                        creater: "小白"
+                        title:"品高XX公告",
+                        time:"2018-7-26 15:50",
+                        creater:"完全正确"
                     }
-				],
-				total: 0,
-				page: 1,
-				listLoading: false,
-				sels: [],//列表选中列
+                ],
+                total: 0,
+                page: 1,
+                listLoading: false,
+                sels: [],//列表选中列
 
-				editFormVisible: false,//编辑界面是否显示
-				editLoading: false,
-				editFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//编辑界面数据
-				editForm: {
-					id: 0,
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				},
+                editFormVisible: false,//编辑界面是否显示
+                editLoading: false,
+                editFormRules: {
+                    name: [
+                        { required: true, message: '请输入姓名', trigger: 'blur' }
+                    ]
+                },
+                //编辑界面数据
+                editForm: {
+                    id: 0,
+                    name: '',
+                    sex: -1,
+                    age: 0,
+                    birth: '',
+                    addr: ''
+                },
 
-				addFormVisible: false,//新增界面是否显示
-				addLoading: false,
-				addFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//新增界面数据
-				addForm: {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				}
+                addFormVisible: false,//新增界面是否显示
+                addLoading: false,
+                addFormRules: {
+                    name: [
+                        { required: true, message: '请输入姓名', trigger: 'blur' }
+                    ]
+                },
+                //新增界面数据
+                addForm: {
+                    name: '',
+                    sex: -1,
+                    age: 0,
+                    birth: '',
+                    addr: ''
+                }
 
-			}
-		},
-		methods: {
-			//性别显示转换
-			formatSex: function (row, column) {
-				return row.sex == 1 ? '男' : row.sex == 0 ? '女' : '未知';
-			},
-			// handleCurrentChange(val) {
-			// 	this.page = val;
-			// 	this.getUsers();
-			// },
-			//获取用户列表
-			getUsers() {
+            }
+        },
+        methods: {
+            handleCurrentChange(val) {
+                this.page = val;
+                this.getUsers();
+            },
+            //获取用户列表
+            getUsers() {
 
             },
-			//删除
-			handleDel: function (index, row) {
-				this.$confirm('确认删除该记录吗?', '提示', {
-					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { id: row.id };
-					removeUser(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getUsers();
-					});
-				}).catch(() => {
+            //删除
+            handleDel: function (index, row) {
+                this.$confirm('确认删除该记录吗?', '提示', {
+                    type: 'warning'
+                }).then(() => {
+                    this.listLoading = true;
+                    //NProgress.start();
+                    let para = { id: row.id };
+                    removeUser(para).then((res) => {
+                        this.listLoading = false;
+                        //NProgress.done();
+                        this.$message({
+                            message: '删除成功',
+                            type: 'success'
+                        });
+                        this.getUsers();
+                    });
+                }).catch(() => {
 
-				});
-			},
-			//显示编辑界面
-			handleEdit: function (index, row) {
-				this.editFormVisible = true;
-				this.editForm = Object.assign({}, row);
-			},
-			//显示新增界面
-			handleAdd: function () {
-				this.addFormVisible = true;
-				this.addForm = {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				};
-			},
-			//编辑
-			editSubmit: function () {
-				this.$refs.editForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.editLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.editForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							editUser(para).then((res) => {
-								this.editLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['editForm'].resetFields();
-								this.editFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			//新增
-			addSubmit: function () {
-				this.$refs.addForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.addLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.addForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							addUser(para).then((res) => {
-								this.addLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['addForm'].resetFields();
-								this.addFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			selsChange: function (sels) {
-				this.sels = sels;
-			},
-			//批量删除
-			batchRemove: function () {
-				var ids = this.sels.map(item => item.id).toString();
-				this.$confirm('确认删除选中记录吗？', '提示', {
-					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { ids: ids };
-					batchRemoveUser(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getUsers();
-					});
-				}).catch(() => {
-
-				});
-			}
-		},
-		mounted() {
-			this.getUsers();
-		}
-	}
+                });
+            },
+            selsChange: function (sels) {
+                this.sels = sels;
+            },
+        },
+        mounted() {
+            this.getUsers();
+        }
+    }
 
 </script>
 
