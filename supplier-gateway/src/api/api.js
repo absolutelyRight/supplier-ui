552ec092128c9f1731abe6108a2f19549fd8f8cd
永航 白 <madridSeven7@163.com>
2018-08-16 11:17:48 +0800

    公告详情页

--- supplier-gateway/src/api/api.js
+++ supplier-gateway/src/api/api.js
@@ -4,7 +4,7 @@ let base = 'http://localhost:8080';
 
 // export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
-export const requestLogin = params => { return axios.get(`${base}/static/user.json`, {}); };
+export const requestLogin = params => { return axios.get(`/static/user.json`, {}); };
 export const register = params => { return axios.post(`${base}/supplier/register`,params).then(res => res.data);};
 
 // export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
